ce880dac5e170e437d382ce3bd4ea9952f8bd649
ALEKSEY KURYLEV <alekseykurylev@MacBook-Air-ALEKSEY.local>
2022-09-30 17:24:09 +0300

    feat: mock data

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/App';
+import App from './components/app/app';
+import './index.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
