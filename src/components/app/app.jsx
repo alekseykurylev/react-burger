8251a4eedab4539c4bc8ae68da27792bc4b47f81
ALEKSEY KURYLEV <alekseykurylev@MacBook-Air-ALEKSEY.local>
2022-10-06 15:13:28 +0300

    feat: fetch api ingredients

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,43 @@
+import "../../styles/global.module.scss";
 import AppHeader from "../app-header/app-header";
 import Constructor from "../page/constructor/constructor";
-import "../../styles/global.module.scss";
-import { data } from "../../utils/data";
+import { API_INGREDIENTS } from "../../utils/const";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch(API_INGREDIENTS)
+      .then((res) => res.json())
+      .then((obj) => {
+        setIngredients(obj.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
+
   return (
     <>
       <AppHeader />
       <main>
-        <Constructor data={data} />
+        {loading ? (
+          "Загрузка..."
+        ) : error ? (
+          "Ошибка сервера, зайдите позже"
+        ) : ingredients.length <= 0 ? (
+          "Извините, но у нас закончились продукты, зайдите позже."
+        ) : (
+          <Constructor data={ingredients} />
+        )}
       </main>
     </>
   );
-}
+};
 
 export default App;
