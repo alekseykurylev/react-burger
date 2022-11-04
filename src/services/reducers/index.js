2524d72e8a7e14d4b33eb495195f6ac77f4669f8
alekseykurylev <alekseykurylev@yandex.ru>
2022-11-04 12:45:34 +0300

    fix: reducer

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -4,13 +4,11 @@ import ingredientsReducer from "../slices/ingredients";
 import orderReducer from "../slices/order";
 import passwordResetReducer from "../slices/password-reset";
 import authReducer from "../slices/auth";
-import modalReducer from "../slices/modal";
 
 const rootReducer = combineReducers({
   ingredients: ingredientsReducer,
   burger: burgerReducer,
   order: orderReducer,
-  modal: modalReducer,
   passwordReset: passwordResetReducer,
   auth: authReducer,
 });
