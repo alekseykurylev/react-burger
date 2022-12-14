1ed37ce07567a4af12a723d643b4ed50bd663ad2
alekseykurylev <alekseykurylev@yandex.ru>
2022-12-14 22:23:43 +0300

    homepage

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
-import './index.scss'
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import { BrowserRouter as Router } from "react-router-dom";
-
+import ReactDOM from "react-dom/client";
+import App from "./components/app/app";
+import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </Provider>
 );
